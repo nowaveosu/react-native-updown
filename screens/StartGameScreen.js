298705c0c0c02df0814dd10f108c7b6a18d316e4
nowaveosu <nowaveosu@gmail.com>
2024-10-08 23:56:30 +0900

    [Add] View 1차

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,10 @@
 import { TextInput, View } from "react-native";
-
+import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View>
       <TextInput />
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
