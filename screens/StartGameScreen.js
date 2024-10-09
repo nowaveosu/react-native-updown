89de461bb2b9ba075439ba54480bf665c16b4873
nowaveosu <nowaveosu@gmail.com>
2024-10-09 16:41:33 +0900

    [Style] 그림자 추가

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -14,8 +14,15 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     marginTop: 100,
+    marginHorizontal: 24,
     padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
   },
 });
