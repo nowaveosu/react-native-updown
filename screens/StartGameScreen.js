cd51cf34c78afa35bed7fa33bf089e1578c1d990
nowaveosu <nowaveosu@gmail.com>
2024-10-09 16:33:58 +0900

    [Add] flex 스타일 적용

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,21 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    marginTop: 100,
+    padding: 16,
+  },
+});
