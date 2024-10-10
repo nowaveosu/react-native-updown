0b76f3eaecbc3899b151fe65af3dbd3f55691566
nowaveosu <nowaveosu@gmail.com>
2024-10-10 13:35:57 +0900

    [Add] maxLength = {2}

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -27,11 +27,13 @@ const styles = StyleSheet.create({
   },
   numberInput: {
     height: 50,
+    width: 50,
     fontSize: 32,
     borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
     color: "#ddb52f",
     marginVertical: 8,
     fontWeight: "bold",
+    textAlign: "center",
   },
 });
