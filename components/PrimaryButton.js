8647ce25d95df45f666110cd88d606ffe87527bb
nowaveosu <nowaveosu@gmail.com>
2024-10-25 19:57:00 +0900

    [Style] 버튼 스타일링

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native";
-
-function PrimaryButton(props) {
+import { View, Text, Pressable, StyleSheet } from "react-native";
+// react native에는 상속계념이 없다
+function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Pressed!");
+  }
   return (
-    <View>
-      <Text>{props.children}</Text>
-    </View>
+    <Pressable onPress={pressHandler}>
+      <View style={styles.container}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#72063c",
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    margin: 4,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
