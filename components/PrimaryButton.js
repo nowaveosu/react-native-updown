5692dea6755ee89247dbc613bca5d3d3a4f06360
nowaveosu <nowaveosu@gmail.com>
2024-11-01 16:57:42 +0900

    [Styles] 전역컬러지정

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,44 +1,48 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
+import Colors from "../constants/colors";
 // react native에는 상속계념이 없다
 function PrimaryButton({ children, onPress }) {
-  return (
-    <View style={styles.buttonOuterContainer}>
-      <Pressable
-        style={({ pressed }) =>
-          pressed
-            ? [styles.buttonInnerContainer, styles.pressed]
-            : styles.buttonInnerContainer
-        }
-        onPress={onPress}
-        android_ripple={{ color: "#640233" }}
-      >
-        <Text style={styles.buttonText}>{children}</Text>
-      </Pressable>
-    </View>
-  );
+	return (
+		<View style={styles.buttonOuterContainer}>
+			<Pressable
+				style={({ pressed }) =>
+					pressed
+						? [
+								styles.buttonInnerContainer,
+								styles.pressed,
+						  ]
+						: styles.buttonInnerContainer
+				}
+				onPress={onPress}
+				android_ripple={{ color: Colors.primary600 }}
+			>
+				<Text style={styles.buttonText}>{children}</Text>
+			</Pressable>
+		</View>
+	);
 }
 
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-  buttonOuterContainer: {
-    borderRadius: 28,
-    margin: 4,
-    overflow: "hidden",
-  },
-  buttonInnerContainer: {
-    backgroundColor: "#72063c",
-    borderRadius: 28,
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    margin: 4,
-    elevation: 2,
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
-  },
-  pressed: {
-    opacity: 0.75,
-  },
+	buttonOuterContainer: {
+		borderRadius: 28,
+		margin: 4,
+		overflow: "hidden",
+	},
+	buttonInnerContainer: {
+		backgroundColor: Colors.primary500,
+		borderRadius: 28,
+		paddingVertical: 8,
+		paddingHorizontal: 16,
+		margin: 4,
+		elevation: 2,
+	},
+	buttonText: {
+		color: "white",
+		textAlign: "center",
+	},
+	pressed: {
+		opacity: 0.75,
+	},
 });
