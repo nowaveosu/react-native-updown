5fa896b8e39db90d0a97df256743ba866e87ea41
nowaveosu <nowaveosu@gmail.com>
2024-12-08 19:40:19 +0900

    [Add] custom font 추가

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,5 @@
 import { Text, StyleSheet } from "react-native";
+
 import Colors from "../../constants/colors";
 
 function Title({ children }) {
@@ -9,8 +10,9 @@ export default Title;
 
 const styles = StyleSheet.create({
 	title: {
+		fontFamily: "open-sans-bold",
 		fontSize: 18,
-		fontWeight: "bold",
+
 		color: "white",
 		textAlign: "center",
 		borderWidth: 2,
