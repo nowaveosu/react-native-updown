0c4cf184dd3e00c1d9d2a584c61ef08107212ee1
nowaveosu <nowaveosu@gmail.com>
2024-12-13 18:09:33 +0900

    [Add] start new game 버튼활성화

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import AppLoading from "expo-app-loading";
 export default function App() {
 	const [userNumber, setUserNumber] = useState();
 	const [gameIsOver, setGameIsOver] = useState(true);
+	const [guessRounds, setGuessRounds] = useState(0);
 
 	const [fontsLoaded] = useFonts({
 		"open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
@@ -34,6 +35,12 @@ export default function App() {
 	function gameOverHandler() {
 		setGameIsOver(true);
 	}
+
+	function startNewGameHandler() {
+		setUserNumber(null);
+		setGuessRounds(0);
+	}
+
 	let screen = (
 		<StartGameScreen onPickNumber={pickedNumberHandler} />
 	);
@@ -47,7 +54,13 @@ export default function App() {
 	}
 
 	if (gameIsOver && userNumber) {
-		screen = <GameOverScreen />;
+		screen = (
+			<GameOverScreen
+				userNumber={userNumber}
+				roundsNumber={guessRounds}
+				onStartNewGame={startNewGameHandler}
+			/>
+		);
 	}
 	return (
 		<LinearGradient
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,8 +1,12 @@
 import { View, Image, StyleSheet, Text } from "react-native";
 import Title from "../components/ui/Title";
 import { Colors } from "react-native/Libraries/NewAppScreen";
-
-function GameOverScreen() {
+import PrimaryButton from "../components/ui/PrimaryButton";
+function GameOverScreen({
+	roundsNumber,
+	userNumber,
+	onStartNewGame,
+}) {
 	return (
 		<View style={styles.rootContainer}>
 			<Title>GAME OVER!</Title>
@@ -12,9 +16,17 @@ function GameOverScreen() {
 					source={require("../assets/images/success-2.png")}
 				/>
 			</View>
-			<Text>
-				Your phone needed X rounds to guess the number Y.
+			<Text style={styles.summaryText}>
+				Your phone needed
+				<Text style={styles.highlight}>
+					{roundsNumber}
+				</Text>{" "}
+				rounds to guess the number
+				<Text style={styles.highlight}>{userNumber}</Text>.
 			</Text>
+			<PrimaryButton onPress={onStartNewGame}>
+				start new game
+			</PrimaryButton>
 		</View>
 	);
 }
@@ -39,5 +51,15 @@ const styles = StyleSheet.create({
 	image: {
 		width: "100%",
 		height: "100%",
+	},
+	summaryText: {
+		fontFamily: "open-sans",
+		fontSize: 24,
+		textAlign: "center",
+		marginVertical: 24,
+	},
+	highlight: {
+		fontFamily: "open-sans-bold",
+		color: Colors.primary500,
 	},
 });
