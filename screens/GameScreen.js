28654a26d9c4363a79a2b92b85d278e74011031a
nowaveosu <nowaveosu@gmail.com>
2024-12-13 19:42:35 +0900

    [Add] flatlist 적용

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	Alert,
+	FlatList,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import Title from "../components/ui/Title";
@@ -103,9 +109,16 @@ function GameScreen({ userNumber, onGameOver }) {
 				</View>
 			</Card>
 			<View>
-				{guessRounds.map((guessRound) => (
+				{/* {guessRounds.map((guessRound) => (
 					<Text key={guessRound}>{guessRound}</Text>
-				))}
+				))} */}
+				<FlatList
+					data={guessRounds}
+					renderItem={(itemData) => (
+						<Text>{itemData.item}</Text>
+					)}
+					keyExtractor={(item) => item}
+				/>
 			</View>
 		</View>
 	);
